478c2a8482d3cd83d4635ce67dfd978410d6be9a
Simeon Chaos <simeon.chaos@gmail.com>
2012-11-09 21:53:40 +0800

    cps(exp, cont), fcont become a global variable.
    fcont don't pass by parameter, becomes a global variable.
    or, not, fail, if, begin, and, succeed pass tests.

--- dao/demosolvecont.py
+++ dao/demosolvecont.py
@@ -37,65 +37,65 @@ def demo1():
 #demo1()
 
 def demo():
-  #print '===================================='
-  #print (or_, 1, 2, 3)
-  #solve((or_, 1, 2, 3))
+  print '===================================='
+  print (or_, 1, 2, 3)
+  solve((or_, 1, 2, 3))
   
-  #print '===================================='
-  #print 'solve_all:', (or_, 1, 2, 3)
-  #solve_all((or_, 1, 2, 3))
+  print '===================================='
+  print (or_, (print_, 1), (print_, 2))
+  solve((or_, (print_, 1), (print_, 2)))
   
-  #print '===================================='
-  #print (findall, (or_, 1))
-  #solve((findall, (or_, 1)))
+  print '===================================='
+  print 'solve_all:', (or_, 1, 2, 3)
+  solve_all((or_, 1, 2, 3))
+  
+  print '===================================='
+  print (findall, (or_, 1))
+  solve((findall, (or_, 1)))
   
   print '===================================='
   print (findall, (or_, 1, 2))
   solve((findall, (or_, 1, 2)))
   
-  #print '===================================='
-  #print (findall, (or_, 1, 2, 3))
-  #solve((findall, (or_, 1, 2, 3)))
+  print '===================================='
+  print (findall, (or_, 1, 2, 3))
+  solve((findall, (or_, 1, 2, 3)))
   
-  #print '===================================='
-  #print (findall, (or_, 1, 2, 3, 4))
-  #solve((findall, (or_, 1, 2, 3, 4)))
+  print '===================================='
+  print (findall, (or_, 1, 2, 3, 4))
+  solve((findall, (or_, 1, 2, 3, 4)))
   
-  #print '===================================='
-  #print (begin, (print_, 1), (print_, 2))
-  #solve((begin, (print_, 1), (print_, 2)))
+  print '===================================='
+  print (begin, (print_, 1), (print_, 2))
+  solve((begin, (print_, 1), (print_, 2)))
  
-  #print '===================================='
-  #print (not_,(begin, (print_, 1), (print_, 2)))
-  #solve((not_,(begin, (print_, 1), (print_, 2))))  
+  print '===================================='
+  print (not_,(begin, (print_, 1), (print_, 2)))
+  solve((not_,(begin, (print_, 1), (print_, 2))))  
 
-  #print '===================================='
-  #print (begin, (findall, (or_, (print_, 1), (print_, 2))), (print_, 3))
-  #solve((begin, (findall, (or_, (print_, 1), (print_, 2))), (print_, 3)))
+  print '===================================='
+  print (begin, (findall, (or_, (print_, 1), (print_, 2))), (print_, 3))
+  solve((begin, (findall, (or_, (print_, 1), (print_, 2))), (print_, 3)))
   
-  #print '===================================='
-  #print (or_, (print_, 1), (print_, 2))
-  #solve((or_, (print_, 1), (print_, 2)))
-  
-demo()
+#demo()
  
 def demo_unify():  
   print '===================================='
   print (unify, 1, 2)
   solve((unify, 1, 2))
+  print
   print (unify, 1, 1)
   solve((unify, 1, 1))
+  print
   print (begin, (unify, _, 1), (unify, _, 2))
   solve((begin, (unify, _, 1), (unify, _, 2)))
+  print
   print (begin, (unify, x, 1))
   solve((begin, (unify, x, 1)))
-  print 'unify in dirty bindings:', (begin, (unify, x, 2))
-  solve((begin, (unify, x, 2)))
-  solvecont.bindings = Bindings()
-  print 'after cleaning bindings:', (begin, (unify, x, 2))
-  solve((begin, (unify, x, 2)))
+  print
   print (begin, (unify, x, 1), (unify, x, 2))
   solve((begin, (unify, x, 1), (unify, x, 2)))
+  print
 
 #demo_unify()
 
@@ -108,7 +108,7 @@ def demo_if():
   print (if_, 0, (print_, 2), (print_, 3))
   solve((if_, 0, (print_, 2), (print_, 3)))
 
-#demo_if()
+demo_if()
 
 def demo_lazy_any():
   print 'demo_lazy_any'
--- dao/solvecont.py
+++ dao/solvecont.py
@@ -12,35 +12,8 @@ class Bindings(dict):
 class Var:
   def __init__(self, name):
     self.name = name
-    
-    def unify(self, other, cont, fcont, occurs_check=False):
-        self = self.deref(bindings)
-        other = deref(other, bindings)
-        if isinstance(self, Var):
-          if occurs_check and contain_var(other, self): return
-          elif isinstance(other, Var) and other is self: 
-            yield True
-            return
-          else:
-            bindings[self] = other
-            yield True
-            try: del bindings[self] # DummyVar oops
-            except: pass
-        elif isinstance(other, Var):
-          if occurs_check and contain_var(self, other): return
-          elif isinstance(self, Var) and other is self: 
-            yield True
-            return
-          else:
-            bindings[other] = self
-            yield True
-            try: del bindings[other] # DummyVar oops
-            except: pass
-        else:
-          for _ in unify(self, other, bindings, occurs_check):
-            yield True 
-            
-  def deref(self):
+                
+  def deref(self, bindings):
     v = bindings[self]
     if v is self: return v
     elif not isinstance(v, Var): return v
@@ -57,7 +30,7 @@ class DummyVar(Var):
   def __init__(self, name='_'): 
     Var.__init__(self, name)
       
-  def deref(self): return self
+  def deref(self, bindings): return self
 
   def getvalue(self, memo):
     try: return memo[self]
@@ -70,274 +43,264 @@ class DummyVar(Var):
         result = getvalue(binding, memo)
         return result
 
-def deref(item):
+def deref(item, bindings):
   try:
     item_deref = item.deref
   except:
     return item
-  return item_deref()
-
-def unify_(x, y, cont, fcont):
-  if isinstance(x, Var):
-    x = deref(x)
-    if isinstance(x, Var):
-      bindings[x] =y
-      def new_fcont(v, fc):
-        del bindings[x]
-        return v, fc
-      return lambda v, fc: cont(True, new_fcont)
-  if isinstance(y, Var):
-    y = deref(y)
-    if isinstance(y, Var):
-      def new_fcont(v, fc):
-        del bindings[y]
-        return v, fc
-      return lambda v, fc: cont(True, new_fcont)
-  if x==y: return cont
-  else: return fcont
-
-def done(v, fc):
-  global finished
-  finished = True
-  print 'Succeed done!'
-  return v, fc
-
-def done_all(v, fc):
-  print 'Succeed done!'
-  return fc(v, end)
-
-def end(v, fc):
-  global finished
-  finished = True
-  print 'Failed at last!'
-  return None, None
-
-def solve(exp, done=done, end=end):
-  global finished
-  finished = False 
-  result = cps(exp, done)
-  result = result(None, end)
-  v, cont = result
-  while not finished:
-    v, cond = cont(v, end)
-  return cont
-
-def solve_all(exp):
-  return solve(exp, done_all, end)
+  return item_deref(bindings)
 
 (begin, print_, if_, quote,
  succeed, fail, and_, or_, findall, not_, unify,
- char, eoi, any, lazy_any, greedy_any) = (
+ settext, char, eoi, any, lazy_any, greedy_any) = (
   'begin', 'print', 'if', 'quote',
   'succeed', 'fail', 'and', 'or', 'findall', "not", "unify",
- 'char', 'eoi', 'any', 'lazy any', 'greedy any')
+ 'settext', 'char', 'eoi', 'any', 'lazy any', 'greedy any')
 
-pos, text = 0, ''
-bindings = Bindings()
+def solve(exp):
+  solver = Solver()
+  return Solver().solve(exp, solver.done, solver.end)
 
-def cps_exps(exps, cont):
-  if len(exps)==0: return lambda fc: cont(fc)(())
-  elif len(exps)==1: return cps(exps[0], cont)
-  else:
-    return cps(exps[0], cps_exps(exps[1:], cont))
+def solve_all(exp):
+  return Solver().solve_all(exp)
 
-def cps(exp, cont):
-  global pos, text
-  if isinstance(exp, int) or isinstance(exp, str):
-    def atom_cont(v, fc):
+class Solver:
+  def __init__(self):
+    self.pos, self.text = 0, ''
+    self.bindings = Bindings()
+    
+  def solve(self, exp, done, end):
+    self.finished = False
+    self.fcont = end
+    return self.cps(exp, done)
+    #result = result(None)
+    #cont = result
+    #while not finished:
+      #cond = cont(v)
+    #return cont
+  
+  def solve_all(self, exp):
+    return self.solve(exp, self.done_all, self.end)
+  
+  def done_all(self, v):
+    print 'Succeed done!'
+    self.fcont(v)
+      
+  def end(self, v):
+    self.finished = True
+    print 'Failed at last!'
+
+  def done(self, v):
+    self.finished = True
+    print 'Succeed done!'
+    return v
+  
+  def cps_exps(self, exps, cont):
+    if len(exps)==0: return cont(())
+    elif len(exps)==1: return self.cps(exps[0], cont)
+    else:
+      return self.cps(exps[0], lambda v: self.cps_exps(exps[1:], cont))
+  
+  def cps(self, exp, cont):
+    global pos, text
+    if isinstance(exp, int) or isinstance(exp, str):
       print exp
-      return cont(exp, fc)
-    return atom_cont
-  
-  elif isinstance(exp, Var): 
-    def var_cont(v, fc):      
-      return cont(exp.deref(bindings), fc)
-    return var_cont
-  
-  elif isinstance(exp, list) or isinstance(exp, tuple):
+      return cont(exp)
     
-    if exp[0]==quote: 
-      def quote_cont(v, fc): 
-        return cont(exp[1], fc)
-      return quote_cont
+    elif isinstance(exp, Var):
+      print exp
+      return cont(exp.deref(bindings))
     
-    if exp[0]==begin: # and is equal to begin
-      return cps_exps(exp[1:], cont)
-    
-    if exp[0]==if_: # (if x y z)
-      def if_cont(v, fc):
-        if(v): return cps(exp[2], cont)(v, fc)
-        else: return cps(exp[3], cont)(v, fc)
-      return cps(exp[1], if_cont)
-    
-    elif exp[0]==print_:
-      def print_cont(v, fc):
+    elif isinstance(exp, list) or isinstance(exp, tuple):
+      
+      if exp[0]==quote: 
+        print quote, exp[1]
+        return cont(exp[1])
+      
+      if exp[0]==begin: # and is equal to begin
+        return self.cps_exps(exp[1:], cont)
+      
+      if exp[0]==if_: # (if x y z)
+        def if_cont(v):
+          if(v): return self.cps(exp[2], cont)
+          else: return self.cps(exp[3], cont)
+        return self.cps(exp[1], if_cont)
+      
+      elif exp[0]==print_:
         print ','.join([str(x) for x in exp[1:]])
-        return cont(v, fc)
-      return print_cont
-    
-    elif exp[0]==succeed:
-      #def succeed_cont(v, fc):
-        #return cont(v, fc)
-      #return succeed_cont
-      return cont
-    
-    elif exp[0]==fail: #lambda fc: fc
-      #def fail_cont(v, fc):
-        #def fail_fun(v):
-          #return v, fc
-        #return fail_fun
-      #return fail_cont
-      return lambda v, fc: (v, fc)
-
-    elif exp[0]==not_:
-      #def not_cont(v, fc):
-        #return cps(exp[1], fc)(None, cont)
-      #return not_cont
-      return lambda v, fc: cps(exp[1], fc)(None, cont)
-     
-    if exp[0]==and_: # and is equal to begin
-      return cps(exp[1], cps(exp[2], cont))
-    
-    elif exp[0]==or_:
-      if len(exp)==1: 
-        return (), cont
-      elif len(exp)==2: 
-        return cps(exp[1], cont)
-      elif len(exp)==3:  
-        return cps(exp[1], lambda v, fc: cont(v, cps(exp[2], lambda v, fc2: cont(v, fc2))))
-      else: 
-        return cps(exp[1], lambda v, fc: cont(v, cps((or_,)+tuple(exp[2:]), lambda v, fc2: cont(v, fc2))))
-    
-    elif exp[0]==unify: #(unify, x, y)
-      def unify_cont(v, fc):
+        return cont(None)
+      
+      elif exp[0]==succeed:
+        return cont(True)
+      
+      elif exp[0]==fail:
+        return self.fcont(True)
+  
+      elif exp[0]==not_:
+        fcont = self.fcont
+        self.fcont = cont
+        return self.cps(exp[1], fcont)
+       
+      if exp[0]==and_: # and is equal to begin
+        return self.cps(exp[1], self.cps(exp[2], cont))
+      
+      elif exp[0]==or_:
+        if len(exp)==1: 
+          return cont(True)
+        elif len(exp)==2: 
+          return self.cps(exp[1], cont)
+        elif len(exp)==3:
+          fc= self.fcont
+          def or_fcont(v):
+            self.fcont = fc
+            return self.cps(exp[2], cont)
+          self.fcont = or_fcont
+          return self.cps(exp[1], cont)
+        else: 
+          fc= self.fcont
+          def or_fcont(v):
+            self.fcont = fc
+            return self.cps((or_,)+tuple(exp[2:]), cont)
+          self.fcont = or_fcont
+          return self.cps(exp[1], cont)
+      
+      elif exp[0]==unify: #(unify, x, y)
         x, y = exp[1:]
+        fc = self.fcont
         if isinstance(x, Var):
-          x = deref(x)
+          x = deref(x, self.bindings)
           if isinstance(x, Var):
-            bindings[x] = y
-            def unify_fcont(v, fc2):
-              del bindings[x]
-              return v, fc
-            return cont(v, unify_fcont)
+            self.bindings[x] = y
+            def unify_fcont(v):
+              del self.bindings[x]
+              return fc(False)
+            self.fcont = unify_fcont
+            return cont(True)
           else:
-            y = deref(y)
+            y = deref(y, self.bindings)
             if isinstance(y, Var):
-              bindings[y] = x
-              def unify_fcont(v, fc2):
-                try: del bindings[y]
+              self.bindings[y] = x
+              def unify_fcont(v):
+                try: del self.bindings[y]
                 except: pass
-                return v, fc
-              return cont(v, unify_fcont)
+                return fc(False)
+              self.fcont = unify_fcont
+              return cont(True)
         elif isinstance(y, Var):
-          y = deref(y)
+          y = deref(y, self.bindings)
           if isinstance(y, Var):
-            bindings[y] = x
-            def new_fcont(v, fc2):
-              try: del bindings[y]
+            self.bindings[y] = x
+            def unify_fcont(v):
+              try: del self.bindings[y]
               except: pass
-              return v, fc
-            return cont(v, unify_fcont)
-        if x==y: return cont(v, fc)
-        else: return v, fc
-      return unify_cont
-    
-    elif exp[0]==eoi:
-      def eoi_cont(v, fc):
+              return fc(False)
+            self.fcont = unify_fcont
+            return cont(True)
+        if x==y: return cont(True)
+        else: return fc(False)
+      
+      elif exp[0]==settext:
+        fc = self.fcont
+        self.parse_state = 0, exp[1]
+        def settext_fcont(v):
+          self.parse_state = old_parse_state
+          return fc(v)
+        self.fcont = settext_fcont
+        return cont(None)
+        
+      elif exp[0]==eoi:
         print eoi, pos,
         if pos==len(text):
           print 'succeed.'
-          return cont(v, fc)
+          return cont(True)
         print text[pos], 'failed'
-        return v, fc
-      return eoi_cont
-    
-    elif exp[0]==char:
-      def char_cont(v, fc):
-        global pos, text
+        return self.fcont(False)
+      
+      elif exp[0]==char:
+        pos, text = self.parse_state
         print char, pos, 
         if pos==len(text):
           print 'failed'
-          return v, fc
+          return self.fcont(False)
         else:
           print text[pos],
           c = deref(exp[1])
+          fc = self.fcont
           if isinstance(c, str):
             if c==text[pos]:
-              def char_fcont(v, fc2):
+              def char_fcont(v):
                 global pos, text
                 pos -= 1
                 print 'char fcont', pos, text[pos]
-                return v, fc
-              char_fcont.pos = pos
+                return fc(False)
+              self.fcont = char_fcont
+              char_fcont.pos = pos              
               print text[pos]
               pos += 1
-              return cont(v, char_fcont)
+              return cont(True)
             else:
-              return fc2(v, fc)
+              return fc(False)
           elif isinstance(c, Var):
-            def char_fcont(v, fc2):
+            def char_fcont(v):
               global pos, text
               pos -= 1
               print 'char fcont', pos, text[pos]
               try: del bindings[c]
               except: pass
-              return fc2(v, fc)
+              return fc(False)
+            self.fcont = char_fcont
             char_fcont.pos = pos
             bindings[c] = text[pos]
             pos += 1
             print 'succeed.'
-            return cont(v, char_fcont)
+            return cont(True)
           else: raise TypeError(c)
-      return char_cont
-    
-    elif exp[0]==findall: 
-      print findall
-      def findall_cont(v, fc):
-        def findall_next(v, fc2):
+      
+      elif exp[0]==findall: 
+        print findall
+        def findall_next(v):
           print 'findall next'
-          fc #for debug
-          return fc2(v, fc)
-        def findall_done(v, fc2):
+          fc = self.fcont
+          self.fcont = findall_done
+          return fc(v)
+        def findall_done(v):
           print 'findall done'
-          return cont(v, fc)
-        result = cps(exp[1], findall_next)
-        return result(v, findall_done)
-      return findall_cont
+          return cont(v)
+        return self.cps(exp[1], findall_next)
+  
+      elif exp[0]==lazy_any: #lazy any
+        def lazy_any_cont(v):
+            self.fcont = lazy_any_cont
+            self.cps(exp[1], cont)
+        def new_cont(v):
+          self.fcont = lazy_any_cont
+          return cont(v)
+        return new_cont(None)
+         
+      #elif exp[0]==lazy_any: # lazy any, same as above, but use lambda to simplify
+        #def lazy_any_cont(v, fc):
+          #return self.cps(exp[1], lambda v, fc:cont(v, lazy_any_cont))(lambda v, fc:cont(v, fc))(v, fc)
+        #return cont(None, lazy_any_cont)
+      
+      #elif exp[0]==any: # nongreedy any
+        #print any
+        #def any_cont(v, fc):
+          #def fcont(v, fc2):
+            #print 'any fail'
+            #return cont(v, fc)
+          #return self.cps(exp[1], any_cont)(v, fcont)
+        #return any_cont
+      
+      elif exp[0]==any: # nongreedy any, same as above, but use lambda to simplify
+        print any
+        def any_cont(v, fc):
+          return self.cps(exp[1], any_cont)(lambda v, fc2: cont(v, fc))
+        return any_cont
+      
+      elif exp[0]==greedy_any: # greedy any
+        print greedy_any
+        def greedy_any_cont(v, fc):
+            return self.cps(exp[1], greedy_any_cont)(v, cont(v, fc))
+        return greedy_any_cont
 
-    elif exp[0]==lazy_any: #lazy any
-      def lazy_any_cont(v, fc):
-          result = cps(exp[1], new_cont)
-          result = result(v, new_fcont)
-          return result[1](v, fc)
-      def new_fcont(v, fc):
-        return cont(v, fc)
-      def new_cont(v, fc):
-        return cont(v, lazy_any_cont)
-      return new_cont
-       
-    #elif exp[0]==lazy_any: # lazy any, same as above, but use lambda to simplify
-      #def lazy_any_cont(v, fc):
-        #return cps(exp[1], lambda v, fc:cont(v, lazy_any_cont))(lambda v, fc:cont(v, fc))(v, fc)
-      #return cont(None, lazy_any_cont)
-    
-    #elif exp[0]==any: # nongreedy any
-      #print any
-      #def any_cont(v, fc):
-        #def fcont(v, fc2):
-          #print 'any fail'
-          #return cont(v, fc)
-        #return cps(exp[1], any_cont)(v, fcont)
-      #return any_cont
-    
-    elif exp[0]==any: # nongreedy any, same as above, but use lambda to simplify
-      print any
-      def any_cont(v, fc):
-        return cps(exp[1], any_cont)(lambda v, fc2: cont(v, fc))
-      return any_cont
-    
-    elif exp[0]==greedy_any: # greedy any
-      print greedy_any
-      def greedy_any_cont(v, fc):
-          return cps(exp[1], greedy_any_cont)(v, cont(v, fc))
-      return greedy_any_cont
+      
